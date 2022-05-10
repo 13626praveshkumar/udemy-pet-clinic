20f85486f43946ab4a3b95a87b84a26c45ca7855
13626praveshkumar <praveshkumar1347906@gmail.com>
2022-05-10 23:13:12 +0530

    Adding Index page and Controller.Closes #8

---
+++ petClinicDemo/src/main/java/spring/udemy/petClinicDemo/services/map/PetServiceMap.java
@@ -0,0 +1,28 @@
+package spring.udemy.petClinicDemo.services.map;
+
+import spring.udemy.petClinicDemo.model.Pet;
+import spring.udemy.petClinicDemo.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet object) {
+      super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ petClinicDemo/src/main/java/spring/udemy/petClinicDemo/services/map/VetServiceMap.java
@@ -0,0 +1,28 @@
+package spring.udemy.petClinicDemo.services.map;
+
+import spring.udemy.petClinicDemo.model.Vet;
+import spring.udemy.petClinicDemo.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+       super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
