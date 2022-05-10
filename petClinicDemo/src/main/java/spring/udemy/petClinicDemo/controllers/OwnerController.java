bef636a36f5b0593679e84f42345fa8a83277d74
13626praveshkumar <praveshkumar1347906@gmail.com>
2022-05-10 23:39:14 +0530

    Adding Owner Index page and Controller.Closes #12

---
+++ petClinicDemo/src/main/java/spring/udemy/petClinicDemo/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package spring.udemy.petClinicDemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/","/index","/index.html"})
+    public String listOwners()
+    {
+        return "owners/index";
+    }
+}
