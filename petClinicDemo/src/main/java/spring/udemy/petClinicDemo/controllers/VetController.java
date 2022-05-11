11835091167209f27252fa117ec20f70a352bfea
13626praveshkumar <praveshkumar1347906@gmail.com>
2022-05-12 03:22:07 +0530

    List All Vets on vet Index page.Closes #15

--- petClinicDemo/src/main/java/spring/udemy/petClinicDemo/controllers/VetController.java
+++ petClinicDemo/src/main/java/spring/udemy/petClinicDemo/controllers/VetController.java
@@ -1,13 +1,24 @@
 package spring.udemy.petClinicDemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.udemy.petClinicDemo.services.VetService;
 
 @Controller
 public class VetController {
+    VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets()
+    public String listVets(Model model)
     {
-       return "vets/index";
+       model.addAttribute("vets",vetService.findAll());
+        return "vets/index";
     }
 }
