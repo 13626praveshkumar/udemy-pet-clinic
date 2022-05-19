04c473c95120802fa0d68018b521d1221accdec8
13626praveshkumar <praveshkumar1347906@gmail.com>
2022-05-19 22:56:49 +0530

    Speciality Map Service.Closes #32

---
+++ petClinicDemo/src/main/java/spring/udemy/petClinicDemo/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package spring.udemy.petClinicDemo.services;
+
+
+
+;
+import spring.udemy.petClinicDemo.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ petClinicDemo/src/main/java/spring/udemy/petClinicDemo/services/map/SpecialityMapService.java
@@ -0,0 +1,30 @@
+package spring.udemy.petClinicDemo.services.map;
+
+import spring.udemy.petClinicDemo.model.Speciality;
+import spring.udemy.petClinicDemo.services.SpecialityService;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return null;
+    }
+}
